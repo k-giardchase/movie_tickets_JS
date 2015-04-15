f94e9288b60f14a4c780e8be96f83b4223517a5f
Liz Beacham & Kyle Giard-Chase <student@epicodus.com>
2015-04-15 13:35:37 -0700

    Create initial project skeleton.

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $("form#tickets").submit(function(event) {
+      event.preventDefault();
+      
+    });
+});
---
+++ specs/specs.js
@@ -0,0 +1,9 @@
+describe("", function() {
+  describe("", function() {
+    it("", function() {
+      var testFunction = Object.create(Function);
+      testBankAccount.withdraw(50);
+      expect(test).to.equal(-50);
+    });
+  });
+});
